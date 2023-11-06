2a1889e00bd2a147b24e11a58309665692d24ef4
Phukyada <phukyada1234567@gmail.com>
2023-11-06 23:27:51 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./page/Login";
+import Path from "./page/Path";
+import Map from "./page/Map";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/path" element={<Path />} />
+        <Route path="/map" element={<Map />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
